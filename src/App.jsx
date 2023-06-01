405c8eab65587f82254c6c761281fae110c5fcf0
slothicorn <jana.baranov4@gmail.com>
2023-06-01 13:33:05 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import './App.css';
-// import ProductList from './components/ProductList';
-// import ProductBasket from './components/ProductBasket';
-// import { useState } from 'react';
 import OrderForm from './components/OrderForm';
 
 function App() {
-  // const [selectedItems, setSelectedItems] = useState([]);
-
   return (
     <div>
       <OrderForm></OrderForm>
